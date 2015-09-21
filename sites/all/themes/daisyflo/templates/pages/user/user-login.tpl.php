440bc5d0c799ff5b87ae45dd46983cccd103881c
imat <raymart.marasigan@gmail.com>
2015-09-21 19:41:10 +0800

    fb oauth

--- sites/all/themes/daisyflo/templates/pages/user/user-login.tpl.php
+++ sites/all/themes/daisyflo/templates/pages/user/user-login.tpl.php
@@ -1,14 +1,14 @@
 <?php 
 $form['name']['#description'] = "";
 // $form['name']['#prefix']      = "";
-// $form['name']['#attributes']['placeholder'] = t('Email');
-// $form['name']['#title_display'] = 'invisible';
+$form['name']['#attributes']['placeholder'] = t('Email');
+$form['name']['#title_display'] = 'invisible';
 
 $form['pass']['#description'] = "";
 // $form['pass']['#prefix']      = "";
 // $form['pass']['#suffix']      = "";
-// $form['pass']['#attributes']['placeholder'] = t('Password');
-// $form['pass']['#title_display'] = 'invisible';
+$form['pass']['#attributes']['placeholder'] = t('Password');
+$form['pass']['#title_display'] = 'invisible';
 
 $form['actions']['#suffix']   = "";
 $form['actions']['submit']['#value']  = "LOG IN";
@@ -31,52 +31,37 @@ $form['actions']['submit']['#value']  = "LOG IN";
 	<h2 class="no-padding">Login</h2>
 </div>
 
-<div class="login container">
-	<div class="row">
-		<div class="col-md-12 text-center" id="heading">
-			<h2><span class="text-tertiary">Login to</span> Your DaisyFlo Account</h2>
+<div class="jumbotron">
+	<div class="login-container">
+		<div class="col-md-3 hidden-xs no-padding">
 		</div>
-	</div>
-	<div class="row">
-		<div class="col-md-3">
-		</div>
-		<div class="col-md-5">
-			<div class="panel panel-default">
-				<div class="panel-body login-panel-body">
+		<div class="col-md-6 col-xs-12 no-padding">
+			<div class="panel panel-default ">
+				<div class="panel-heading primary-bg-color">
 					<div class="text-center">
-						<img id="dog-logo" src="/sites/all/themes/daisyflo/img/theme/login-dog.jpg" />
+						<h2 class="text-tertiary no-margin already-title">Already Have Account?</h2>
 					</div>
-					
+				</div>
+				<div class="panel-body login-panel-body">
 					<div class="login-field-container">
 						<?php echo render($form['name']);?>
 					</div>
 					<div class="login-field-container">
 						<?php echo render($form['pass']);?>
 					</div>
-					<div class="login-option row">
-						<div class="col-md-6"><input type="checkbox" /><span class=""><u> Keep me logged in</u></span></div>
-						<div class="col-md-6 "><span class="pull-right"><a class="text-muted" href="/user/password">Forgot password?</a></span></div>
-					</div>	
 					<div class="row">
-						<div class="col-sm-12">
+						<div class="col-sm-12 text-center">
 							<?php echo render($form['actions']);?>
+						 <?php print fboauth_action_display('connect'); ?>
 						</div>
 					</div>
 					<?php print drupal_render_children($form);?>
 				</div>
-				<div class="panel-heading">
-					<div class="text-center">
-						<p>
-						<strong>
-							Don't have an account yet? <a href="/user/register" class="text-tertiary">Register here.</a>
-						</strong>
-						</p>
-					</div>
-				</div>
 			</div>
 		</div>
-		<div class="col-md-4">
+		<div class="col-md-3 hidden-xs no-padding">
 		</div>
-	</div>
 </div>
+</div>
+
 
